1e2cd4a17ee0b411d125613ba768a52aded305cf
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-06-01 20:42:54 +0200

    Minor changes

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -2,17 +2,16 @@
 import { ref, computed, onBeforeUnmount } from 'vue';
 
 const props = defineProps({
-  startgame: Function, // Define the startgame prop
+  startgame: Function,
 });
 
-const emits = defineEmits(['startgame', 'resetgame']); // Add 'resetgame' to defineEmits
-
+const emits = defineEmits(['startgame', 'resetgame']);
 
 const player = ref('X');
 const board = ref([
   ['', '', ''],
   ['', '', ''],
-  ['', '', ''],   // 9 squares for the board
+  ['', '', ''],
 ])
 
 const score = ref<{ [key: string]: number }>({
@@ -29,10 +28,10 @@ const calculateWinner = (squares: string[]): string | null => {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]   // lines for winning
+    [2, 4, 6]
   ];
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];  // the lines redefines to a, b and c
+    const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
@@ -46,23 +45,23 @@ const isBoardFull = computed(() => {
   for (let i = 0; i < board.value.length; i++) {
     for (let j = 0; j < board.value[i].length; j++) {
       if (board.value[i][j] === '') {
-        return false; // If any cell is empty, the board is not full
+        return false;
       }
     }
   }
-  return true; // All cells are filled
+  return true;
 });
 
 const isTied = ref(false);
 
 const makeAMove = (x: number, y: number) => {
-  if (!gameStarted.value || winner.value) return;  // Check if game has started and there is no winner
+  if (!gameStarted.value || winner.value) return;
 
-  if (board.value[x][y] !== '') return;    // if the board's value is not equal to an empty box, someone is playing
+  if (board.value[x][y] !== '') return;
 
   board.value[x][y] = player.value;
 
-  player.value = player.value === 'X' ? 'O' : 'X';  //if this is true -> swap it to O - else it's X
+  player.value = player.value === 'X' ? 'O' : 'X';
 
   const currentWinner = calculateWinner(board.value.flat());
   if (currentWinner) {
@@ -114,9 +113,9 @@ const loadGameState = () => {
   }
 };
 
-loadGameState();  // Call this function when the game starts or refreshes
+loadGameState();
 
-onBeforeUnmount(() => {   // Save game state whenever there is a change
+onBeforeUnmount(() => {
   saveGameState();
 });
 
@@ -135,23 +134,26 @@ const startGame = () => {
   <main class="pt-8 text-center bg-gray-800 dark:bg-gray-900 min-h-screen dark:text-white">
     <div class="flex justify-center pr-4 mx-20 mb-8 m-10 ml-40">
       <div class="text-black mx-8 mt-7">
-        <h1 class="mb-0 mr-10 px-8 text-7xl font-bold uppercase rounded bg-yellow-200 shadow-xl shadow-yellow-500/100">Tic</h1>
-        <h1 class="mb-0 mr-10 text-7xl font-bold uppercase rounded mt-2 bg-yellow-200 shadow-xl shadow-yellow-500/100">Tac</h1>
-        <h1 class="mb-0 mr-10 text-7xl font-bold uppercase rounded mt-2 bg-yellow-200 shadow-xl shadow-yellow-500/100">Toe</h1>       
+        <h1 class="mb-0 mr-10 px-8 text-7xl font-bold uppercase rounded bg-yellow-200 shadow-xl shadow-yellow-500/100">Tic
+        </h1>
+        <h1 class="mb-0 mr-10 text-7xl font-bold uppercase rounded mt-2 bg-yellow-200 shadow-xl shadow-yellow-500/100">Tac
+        </h1>
+        <h1 class="mb-0 mr-10 text-7xl font-bold uppercase rounded mt-2 bg-yellow-200 shadow-xl shadow-yellow-500/100">Toe
+        </h1>
       </div>
 
       <div class="flex justify-around mx-20 ">
         <div>
           <h3 class="text-2xl font-bold text-white">
-            {{ gameStarted && (player === 'X' ? playerXName : playerOName) ? (player === 'X' ? playerXName : playerOName) + "'s turn" : "Player X's turn" }}
+            {{ gameStarted && (player === 'X' ? playerXName : playerOName) ? (player === 'X' ? playerXName : playerOName)
+              + "'s turn" : "Player X's turn" }}
           </h3>
 
           <div class="flex flex-col items-center mb-6">
             <div v-for="(row, x) in board" :key="x" class="flex">
 
               <div v-for="(cell, y) in row" :key="y" @click="makeAMove(x, y)"
-                :class="`border border-white w-20 h-20 hover:bg-grey-700 flex items-center
-                                justify-center material-icons-outlined text-4x1 cursor-pointer ${cell === 'X' ? 'text-green-500' : 'text-pink-400'}`">
+                :class="`border border-white w-20 h-20 hover:bg-grey-700 flex items-center justify-center material-icons-outlined text-4x1 cursor-pointer ${cell === 'X' ? 'text-green-500' : 'text-pink-400'}`">
                 {{ cell === 'X' ? 'close' : cell === 'O' ? 'circle' : '' }}
               </div>
             </div>
@@ -163,34 +165,41 @@ const startGame = () => {
 
           <h2 v-if="isTied" class="text-2xl font-bold mb-5 text-amber-200">Tied game!</h2>
 
-          <button v-show="gameStarted || isTied" @click="resetGame" class="px-4 mt-3 py-2 bg-green-500 rounded uppercase font-bold hover:bg-green-600 duration-300">Play again?</button>
+          <button v-show="gameStarted || isTied" @click="resetGame"
+            class="px-4 mt-3 py-2 bg-green-500 rounded uppercase font-bold hover:bg-green-600 duration-300">Play
+            again?</button>
 
         </div>
 
         <div class="mb-8 mx-20">
           <h2 class="text-2xl font-bold text-white mt-14 mb-2 mx-12">Score</h2>
           <div class="text-white">
-            <p>{{ playerXName || 'Player X' }}: {{ score.X }}</p>
-            <p>{{ playerOName || 'Player O' }}: {{ score.O }}</p>
-            <button @click="resetScore" class="px-2 py-1 bg-pink-400 rounded uppercase 
-                font-bold hover:bg-pink-500 duration-300 mt-4 text-black">Reset score</button>
+              <p>{{ playerXName || 'Player X' }}: {{ score.X }}</p>
+              <p>{{ playerOName || 'Player O' }}: {{ score.O }}</p>
+              <button @click="resetScore"
+                class="px-2 py-1 bg-pink-400 rounded uppercase font-bold hover:bg-pink-500 duration-300 mt-4 text-black">Reset
+                score</button>
           </div>
-        </div>        
+        </div>
       </div>
     </div>
     <div class="mt-6">
       <div class="max-w-sm mx-auto">
-          <form class="flex justify-start mt-20">
-            <div class="mr-4">
-              <label for="playerXName" class="mb-2 font-bold text-white">Player X:</label>
-              <input type="text" id="playerXName" v-model="playerXName" required placeholder="Name" class="px-2 py-1 ml-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
-            </div>
-            <div>
-              <label for="playerOName" class="mb-2 font-bold text-white">Player O:</label>
-              <input type="text" id="playerOName" v-model="playerOName" required placeholder="Name" class="px-2 py-1 ml-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
-            </div>
-            <button v-if="!gameStarted && playerXName && playerOName" @click="startGame" class="px-4 mt-0 ml-4 py-2 bg-green-500 rounded uppercase font-bold hover:bg-green-600 duration-300">Start game</button>
-          </form>
+        <form class="flex justify-start mt-20">
+          <div class="mr-4">
+            <label for="playerXName" class="mb-2 font-bold text-white">Player X:</label>
+            <input type="text" id="playerXName" v-model="playerXName" required placeholder="Name"
+              class="px-2 py-1 ml-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+          </div>
+          <div>
+            <label for="playerOName" class="mb-2 font-bold text-white">Player O:</label>
+            <input type="text" id="playerOName" v-model="playerOName" required placeholder="Name"
+              class="px-2 py-1 ml-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+          </div>
+          <button v-if="!gameStarted && playerXName && playerOName" @click="startGame"
+            class="px-4 mt-0 ml-4 py-2 bg-green-500 rounded uppercase font-bold hover:bg-green-600 duration-300">Start
+            game</button>
+        </form>
       </div>
     </div>
   </main>
