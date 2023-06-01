4a105a910643aceed6df3062ec54bdefbd20c340
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-06-01 16:28:43 +0200

    Styling

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -128,20 +128,18 @@ const startGame = () => {
 
 <template>
   <main class="pt-8 text-center bg-gray-800 dark:bg-gray-900 min-h-screen dark:text-white">
-    <div class="flex justify-start mx-20 mb-8">
-      <div class="text-black mx-8 mt-14">
-        <h1 class="mb-0 text-5xl font-bold uppercase bg-yellow-200 shadow-xl shadow-yellow-500/50">Tic</h1>
-        <h1 class="mb-0 text-5xl font-bold uppercase mt-2 bg-yellow-200 shadow-xl shadow-yellow-500/50">Tac</h1>
-        <h1 class="mb-0 text-5xl font-bold uppercase mt-2 bg-yellow-200 shadow-xl shadow-yellow-500/50">Toe</h1>       
+    <div class="flex justify-center pr-4 mx-20 mb-8 m-10 ml-40">
+      <div class="text-black mx-8 mt-7">
+        <h1 class="mb-0 mr-10 px-8 text-7xl font-bold uppercase rounded bg-yellow-200 shadow-xl shadow-yellow-500/100">Tic</h1>
+        <h1 class="mb-0 mr-10 text-7xl font-bold uppercase rounded mt-2 bg-yellow-200 shadow-xl shadow-yellow-500/100">Tac</h1>
+        <h1 class="mb-0 mr-10 text-7xl font-bold uppercase rounded mt-2 bg-yellow-200 shadow-xl shadow-yellow-500/100">Toe</h1>       
       </div>
 
-      <div class="flex justify-around mx-20">
+      <div class="flex justify-around mx-20 ">
         <div>
-          <h3 class="text-1xl font-bold text-white">
+          <h3 class="text-2xl font-bold text-white">
             {{ gameStarted && (player === 'X' ? playerXName : playerOName) ? (player === 'X' ? playerXName : playerOName) + "'s turn" : "Player X's turn" }}
           </h3>
-
-
 
           <div class="flex flex-col items-center mb-6">
             <div v-for="(row, x) in board" :key="x" class="flex">
@@ -165,25 +163,25 @@ const startGame = () => {
         </div>
 
         <div class="mb-8 mx-20">
-          <h2 class="text-1xl font-bold text-white mt-14 mx-12">Score</h2>
+          <h2 class="text-2xl font-bold text-white mt-14 mb-2 mx-12">Score</h2>
           <div class="text-white">
             <p>{{ playerXName || 'Player X' }}: {{ score.X }}</p>
             <p>{{ playerOName || 'Player O' }}: {{ score.O }}</p>
             <button @click="resetScore" class="px-2 py-1 bg-pink-400 rounded uppercase 
-                font-bold hover:bg-pink-500 duration-300 mt-4 text-black">Reset</button>
+                font-bold hover:bg-pink-500 duration-300 mt-4 text-black">Reset score</button>
           </div>
         </div>        
       </div>
     </div>
-    <div class="mt-8">
-      <form class="flex justify-end">
+    <div class="mt-6">
+      <form class="flex justify-start ml-60 mt-20">
         <div class="mr-4">
-          <label for="playerXName" class="mb-2 font-bold text-white">Player X name:</label>
-          <input type="text" id="playerXName" v-model="playerXName" required class="px-2 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <label for="playerXName" class="mb-2 font-bold text-white" >Player X</label>
+          <input type="text" id="playerXName" v-model="playerXName" required placeholder="Name" class="px-2 py-1 ml-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
         </div>
         <div>
-          <label for="playerOName" class="mb-2 font-bold text-white">Player O name:</label>
-          <input type="text" id="playerOName" v-model="playerOName" required class="px-2 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <label for="playerOName" class="mb-2 font-bold text-white">Player O</label>
+          <input type="text" id="playerOName" v-model="playerOName" required placeholder="Name" class="px-2 py-1 ml-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
         </div>
         <button v-if="!gameStarted && playerXName && playerOName" @click="startGame" class="px-4 mt-0 ml-4 py-2 bg-green-500 rounded uppercase font-bold hover:bg-green-600 duration-300">Start game</button>
 
